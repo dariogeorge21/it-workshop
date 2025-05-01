5f427261d327e87c5b7cc5da421469929e461580
Dario George <edu.dariogeorge21@gmail.com>
2025-05-01 22:27:15 +0530

    date and time

--- date/script.js
+++ date/script.js
@@ -1,9 +1,21 @@
+function clock() {
 const now = new Date();
 
 let hour = document.getElementById("hour");
 let minute = document.getElementById("minute");
 let second = document.getElementById("second");
 let millisecond = document.getElementById("millisecond");
-let day = document.getElementsById("day");
-let month = document.getElementsById("month");
-let year
+let day = document.getElementById("day");
+let month = document.getElementById("month");
+let year = document.getElementById("year");
+
+hour.innerHTML = now.getHours();
+minute.innerHTML = now.getMinutes();
+second.innerHTML = now.getSeconds();
+millisecond.innerHTML = now.getMilliseconds();
+day.innerHTML = now.getDate();
+month.innerHTML = now.getMonth() + 1;
+year.innerHTML = now.getFullYear();
+
+}
+setInterval(clock, 50);
