15059d083165daf3841e8e2e3f7c7e0bebcbc422
Dario George <edu.dariogeorge21@gmail.com>
2025-05-01 21:39:14 +0530

    confirmation of password

--- form validation/script.js
+++ form validation/script.js
@@ -2,10 +2,15 @@ function validate() {
     let name=document.getElementById("name").value;
     let email=document.getElementById("email").value;
     let password=document.getElementById("password").value;
+    let cpassword=document.getElementById("cpassword").value;
     let val=true;
 
     if (name="" || email=="" || password=="") {
         alert("All fields are mandatory");
+        val=false;
+    }
+    if (password == cpassword) {
+        alert("Password and Confirm Password should be same");
         val=false;
     }
     return val;
