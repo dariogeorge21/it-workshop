9ff06ebf9966faec1a58ccf7172ddb35e5e65fb2
Dario George <edu.dariogeorge21@gmail.com>
2025-05-01 23:10:39 +0530

    sample

--- form validation/script.js
+++ form validation/script.js
@@ -2,7 +2,7 @@ function validate() {
     let name=document.getElementById("name").value;
     let email=document.getElementById("email").value;
     let password=document.getElementById("password").value;
-    let cpassword=document.getElementById("cpassword").value;
+    let cpassword=document.getElementById("c-password").value;
     let val=true;
 
     if (name="" || email=="" || password=="") {
@@ -12,6 +12,9 @@ function validate() {
     if (password !== cpassword) {
         alert("Password and Confirm Password should be same");
         val=false;
+    }
+    if (val) {
+        alert("Form submitted successfully");
     }
     return val;
 }
